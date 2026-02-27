4677a177de55dcd3be0fd681911800ef7be6f118
web-coder <yasher18@bk.ru>
2026-02-27 11:24:00 +0300

    создал компонент  стилизовал и вывел переменые в html

---
+++ client/src/components/FormFIlter/FormFilter.jsx
@@ -0,0 +1,22 @@
+import './FormFilter.css'
+
+function FormFIlter() {
+
+    return (
+
+        <>
+        
+            <div className="container">
+                <input type="date"></input>
+                <input type="date"></input>
+                <button>Применить</button>
+            </div>
+
+        </>
+
+    )
+
+}
+
+
+export default FormFIlter
